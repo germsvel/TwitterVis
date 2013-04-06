18ba3008be19d5e9d758be36764678313c3dd6a2
German Velasco <germsvel@gmail.com>
2013-04-06 18:03:16 -0400

    initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterVis::Application.config.secret_token = 'b5507c1aaa181ea3a5402dedb71ae157476effa440ac42be5faeb9d8c59c0aa57d8f1afac61d9b239f0af12965d7f97dbdfa48a95a764c63865b996b17c003a4'
