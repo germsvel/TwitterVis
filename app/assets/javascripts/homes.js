016f44095279e186a1efdb76fd424e8d221848df
Sam Selikoff <sam.selikoff@gmail.com>
2013-04-06 21:59:29 -0400

    Updated js

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,20 +1,20 @@
-// dataset1
-var dataset1 = new Array();
-for (var day=10; day < 30; day++) { 
-    for (hour=10; hour < 24; hour++) { 
-        dataset1.push({
-            'date': '2013-02-'+day+' '+hour+':00:00',
-            'running': Math.floor(Math.random()*200),
-            'waiting': Math.floor(Math.random()*100),
-        });
-    }
-}
+// // dataset1
+// var dataset1 = new Array();
+// for (var day=10; day < 30; day++) { 
+//     for (hour=10; hour < 24; hour++) { 
+//         dataset1.push({
+//             'date': '2013-02-'+day+' '+hour+':00:00',
+//             'running': Math.floor(Math.random()*200),
+//             'waiting': Math.floor(Math.random()*100),
+//         });
+//     }
+// }
 
-var categoryData = [
-	{'category': 'tweetsLow', 'count': 432},
-	{'category': 'tweetsMed', 'count': 155},
-	{'category': 'tweetsHigh', 'count': 200}
-];
+// var categoryData = [
+// 	{'category': 'tweetsLow', 'count': 432},
+// 	{'category': 'tweetsMed', 'count': 155},
+// 	{'category': 'tweetsHigh', 'count': 200}
+// ];
 
 var timeSeriesChart = new TimeSeriesChart().showOverlay(true);
 var barChart = new BarChart();
@@ -35,15 +35,35 @@ $(document).ready(function($) {
 });
 
 function drawDashboard() {
-	d3.select('#timeSeriesChart')
-		.data([dataset1])
-		.call(timeSeriesChart);
 
-	d3.select('#barChart')
-		.data([categoryData])
-		.call(barChart);
+	$.get(document.URL + '/data', function(rawData) {
 
-	d3.select('#pieChart')
-		.data([categoryData])
-		.call(pieChart);
+		aggregateData = {};		
+		rawData.forEach(function(d) {
+            aggregateData[d.substr(0,10) + ' ' + d.substr(12,5)]++;
+            // data.push(
+            // 	{
+            // 		'date': d.substr(0,10) + ' ' + d.substr(12,8),
+            // 		'count': 
+            // 	}
+            // );
+        });
+
+        console.log(aggregateData);
+
+		// d3.select('#timeSeriesChart')
+		// 	.data([dataset1])
+		// 	.call(timeSeriesChart);
+
+		// d3.select('#barChart')
+		// 	.data([categoryData])
+		// 	.call(barChart);
+
+		// d3.select('#pieChart')
+		// 	.data([categoryData])
+		// 	.call(pieChart);
+
+	});
+
+
 }
